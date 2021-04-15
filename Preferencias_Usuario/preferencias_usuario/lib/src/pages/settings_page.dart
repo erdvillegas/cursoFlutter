b9201bf93b5539e530390817d983cde5560a7858
Erik Villegas <erdvillegas@gmail.com>
2021-04-15 01:42:56 -0500

    Ejercico de preferencias

--- Preferencias_Usuario/preferencias_usuario/lib/src/pages/settings_page.dart
+++ Preferencias_Usuario/preferencias_usuario/lib/src/pages/settings_page.dart
@@ -1,8 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:preferencias_usuario/widgets/menu_widget.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static final String routeName = 'settings';
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  bool _colorSecundario = false;
+  int _genero = 1;
+  String _nomnre = 'Erik';
+
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    cargarPref();
+    super.initState();
+    _textController = new TextEditingController(text: _nomnre);
+  }
+
+  cargarPref() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    _genero = prefs.getInt('genero');
+    setState(() {});
+  }
+
+  _setSelectedRadio(int valor) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('genero', valor);
+    _genero = valor;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +47,35 @@ class SettingsPage extends StatelessWidget {
             child: Text('Settings',
                 style: TextStyle(fontSize: 45.0, fontWeight: FontWeight.bold)),
           ),
-          Divider()
+          Divider(),
+          SwitchListTile(
+              value: _colorSecundario,
+              title: Text('Color secundario'),
+              onChanged: (value) {
+                this._colorSecundario = value;
+                setState(() {});
+              }),
+          RadioListTile(
+            value: 1,
+            title: Text('Masculino'),
+            onChanged: _setSelectedRadio,
+            groupValue: _genero,
+          ),
+          RadioListTile(
+            value: 2,
+            title: Text('Femenino'),
+            onChanged: _setSelectedRadio,
+            groupValue: _genero,
+          ),
+          Divider(),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(
+                      labelText: 'Nombre',
+                      helperText: 'Nombre del propietario'),
+                  onChanged: (valor) {})),
         ]));
   }
 }
